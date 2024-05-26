6bbd9257719eed1623bbd92c6f6ed9bc427d5280
Edmund Yin <130507371+edmundyin@users.noreply.github.com>
2024-05-26 12:20:08 -0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  plugins: [reactRefresh()],
+  build: {
+    // outDir,
+    // emptyOutDir: true,
+    // rollupOptions: {
+    //   input: {
+    //     App: resolve(root, 'index.html'),
+    //     home: resolve(root, 'home', 'index.html'),
+    //     landing: resolve(root, 'landing', 'index.html'),
+    //   }
+    // }
+  }
+})
